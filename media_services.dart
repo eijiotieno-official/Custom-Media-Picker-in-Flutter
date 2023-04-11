4bc0e2236614ca637e483ff34ebfbd0495006f9c
Eiji Otieno <130438645+eijiotieno-official@users.noreply.github.com>
2023-04-11 10:12:53 +0300

    Add files via upload

---
+++ media_services.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:photo_manager/photo_manager.dart';
+
+class MediaServices {
+  Future loadAlbums(RequestType requestType) async {
+    var permission = await PhotoManager.requestPermissionExtend();
+    List<AssetPathEntity> albumList = [];
+
+    if (permission.isAuth == true) {
+      albumList = await PhotoManager.getAssetPathList(
+        type: requestType,
+      );
+    } else {
+      PhotoManager.openSetting();
+    }
+
+    return albumList;
+  }
+
+  Future loadAssets(AssetPathEntity selectedAlbum) async {
+    List<AssetEntity> assetList = await selectedAlbum.getAssetListRange(
+      start: 0,
+      // ignore: deprecated_member_use
+      end: selectedAlbum.assetCount,
+    );
+    return assetList;
+  }
+}
