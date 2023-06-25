70ec9d151d7df5985bb6242130a536d7fcb8d23c
Eiji Otieno <130438645+eijiotieno-official@users.noreply.github.com>
2023-06-25 11:04:42 +0300

    Update home.dart

--- home.dart
+++ home.dart
@@ -23,7 +23,7 @@ class _HomeState extends State<Home> {
       context,
       MaterialPageRoute(
         builder: (context) {
-          return MediaPicker(maxCount, requestType);
+          return MediaPicker(maxCount, requestType, selectedAssetList);
         },
       ),
     );
